8ae11bc7ea2e0138713508dfe15587f6642b362d
GeekIntoSlide <Kraghuvanshi435@gmail.com>
2022-10-06 00:16:33 +0530

    Create pages routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <Router>
+    <Routes>
+      <Route>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/offers" element={<Offers/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<Signup/>}/>
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
