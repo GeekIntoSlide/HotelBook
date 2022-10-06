82d0ee1af18ee1646ab61bf922b06e4fa125cbb2
GeekIntoSlide <Kraghuvanshi435@gmail.com>
2022-10-06 23:35:01 +0530

    Design sign-in sign-up forgot-fassword Ui

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import SigninPhoto from '../static/sign-in.jpg';
+import { useNavigate } from 'react-router';
 
 export default function Signup() {
+  const navigate=useNavigate();
   return (
-    <div>Sign up</div>
+    <React.Fragment>
+      <div className='text-center'>
+        <h1 className="text-[40px] font-black">Sign Up</h1>
+      </div>
+      <div className='flex justify-between ml-[40px] mt-[80px]'>
+        <div>
+          <img src={SigninPhoto} className="h-[400px] rounded-[20px]"/>
+        </div>
+        <div className='mr-[300px] mt-[25px]'>
+        <input type="text" placeholder='Full Name' className={`block w-[400px] border-2 mb-[10px] h-[40px] border-sky-500` } />
+          <input type="email" placeholder='Email address' className={`block w-[400px] border-2 mb-[10px] h-[40px] border-sky-500` } />
+          <input type="password" placeholder="Password" className='block w-[400px] border-2 mb-[10px] h-[40px] border-sky-500'/>
+          <span className='mb-[10px] cursor-pointer'>Have an account?</span>
+          <span className='ml-[5px] text-red-400 mb-[10px] cursor-pointer' onClick={()=>navigate('/sign-in')}>Sign-In</span>
+          <span className='ml-[80px] mb-[10px] text-cyan-400 mb-[10px] cursor-pointer' onClick={()=>navigate('/forgot-password')}>Forgot Password? </span>
+          <button  type='submit' className='bg-sky-500 block w-[400px] h-[40px] mb-[10px] mt-[10px] text-white'>Sign-In</button>
+          <h1 className='text-center font-bold'>OR</h1>
+          <button type="submit" className="w-[400px] bg-red-500 h-[40px] mt-[10px]"><i class="fa-brands fa-google"></i> Continue With Google</button>
+        </div>
+      </div>
+
+    </React.Fragment>
   )
 }
